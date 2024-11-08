19c1d2abac3b6ffc605b0a1e2050fd9b63e8afdb
Teddykavooh <teddykavooh@gmail.com>
2024-11-08 15:36:32 +0300

    Configure and test app, api urls

--- py_client/basic.py
+++ py_client/basic.py
@@ -2,14 +2,14 @@ import requests
 
 endpoint_ok = "https://httpbin.org/status/200"
 endpoint = "https://httpbin.org/anything"
-endpoint_django = "http://127.0.0.1:8000/"
+endpoint_django = "http://127.0.0.1:8000/api"
 
 get_response = requests.get(endpoint_django) # HTTP Request
-print(get_response.text) # print source code
 
 # HTTP Request -> HTML
 print(get_response.text) # print source code
 # REST API HTTP Request -> JSON
-print(get_response.json) # print json
+print(get_response.json()) # print json
+print(get_response.json()['message']) # print json message
 # Django localhost
 print(get_response.status_code)
