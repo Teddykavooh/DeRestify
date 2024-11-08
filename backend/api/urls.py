9049dd37e786805fa2450d9ad2b5146f51138486
Teddykavooh <teddykavooh@gmail.com>
2024-11-08 19:12:55 +0300

    Request handling

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -4,4 +4,3 @@ from . import views
 urlpatterns = [
     path('', views.api_home)
 ]
-
--- backend/api/views.py
+++ backend/api/views.py
@@ -1,6 +1,22 @@
 # from django.shortcuts import render -> Default
+import json
 from django.http import JsonResponse
 
 # Create your views here.
-def api_home(requests, *args, **kwargs):
-    return JsonResponse({"message": "Hi there, this is your Django API response!!"})
+def api_home(request, *args, **kwargs):
+    # return JsonResponse({"message": "Hi there, this is your Django API response!!"})
+    # request -> HttpRequest -> Django object
+    print("Rada is which!")
+    print(request.GET)
+    print(request.POST)
+    body = request.body # byte str of json data
+    data = {}
+    try:
+        data = json.loads(body) # str -> py dict
+    except:
+        pass
+    print(data)
+    data['params'] = dict(request.GET)
+    data['headers'] = dict(request.headers)
+    data['content_type'] = request.content_type
+    return JsonResponse(data)
