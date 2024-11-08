6ced66ce30f8b676f512a8bbb0b3d9c7a5ef1e58
Teddykavooh <teddykavooh@gmail.com>
2024-11-08 15:35:26 +0300

    Create an app, api

---
+++ backend/api/views.py
@@ -0,0 +1,6 @@
+# from django.shortcuts import render -> Default
+from django.http import JsonResponse
+
+# Create your views here.
+def api_home(requests, *args, **kwargs):
+    return JsonResponse({"message": "Hi there, this is your Django API response!!"})
