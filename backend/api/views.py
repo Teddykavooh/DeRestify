63cd17d6bd08393dbf21fad3fc7209afc7d7bf63
Teddykavooh <teddykavooh@gmail.com>
2024-11-12 15:11:59 +0300

    Convert api_home view into a DRF view

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,32 +1,16 @@
-# from django.shortcuts import render -> Default
-import json
-from django.http import JsonResponse
 from products.models import Product
 from django.forms.models import model_to_dict
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
 # Create your views here.
+@api_view(["GET"])
 def api_home(request, *args, **kwargs):
-    # return JsonResponse({"message": "Hi there, this is your Django API response!!"})
-    # request -> HttpRequest -> Django object
-    # print("Rada is which!")
-    # print(request.GET)
-    # print(request.POST)
-    # body = request.body # byte str of json data
-    # data = {}
-    # try:
-    #     data = json.loads(body) # str -> py dict
-    # except:
-    #     pass
-    # print(data)
-    # data['params'] = dict(request.GET)
-    # data['headers'] = dict(request.headers)
-    # data['content_type'] = request.content_type
+    """
+    DRF API View
+    """
     model_data = Product.objects.all().order_by("?").first()
     data = {}
     if model_data:
-        # data['id'] = model_data.id
-        # data['title'] = model_data.title
-        # data['content'] = model_data.content
-        # data['price'] = model_data.price
-        data = model_to_dict(model_data, fields=['id', 'title'])
-    return JsonResponse(data)
+        data = model_to_dict(model_data, fields=['id', 'title', 'price'])
+    return Response(data)
