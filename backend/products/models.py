e111ba57c74d3a48d577f5506e5371d5a07da54f
Teddykavooh <teddykavooh@gmail.com>
2024-11-12 19:21:44 +0300

    Code formatting

--- backend/products/models.py
+++ backend/products/models.py
@@ -2,14 +2,19 @@ from django.db import models
 
 # Create your models here.
 class Product(models.Model):
+    """
+    Product Object
+    """
     title = models.CharField(max_length=120)
     content = models.TextField(blank=True, null=True)
     price = models.DecimalField(max_digits=15, decimal_places=2, default=99.99)
 
     @property
     def sale_price(self):
+        """Product Sale Price property"""
         return "%.2f" %(float(self.price) * 0.8)
 
     @property
     def get_discount(self):
+        """Product Discount Property"""
         return "122"
